26fa4941b6ab79a83a5f862d9cca66524adbc736
srivishnu2101-1 <srivishnu.s1@thoughtworks.com>
2020-02-24 21:49:28 +0530

    added email preferences

--- app/src/main/java/com/example/e_recovery/operationActivity.java
+++ app/src/main/java/com/example/e_recovery/operationActivity.java
@@ -1,15 +1,13 @@
 package com.example.e_recovery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.time.Instant;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class operationActivity extends AppCompatActivity {
     private Button vehicle, patientData, emergency;
@@ -18,9 +16,9 @@ public class operationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operation);
-        vehicle = (Button) findViewById(R.id.vehicle);
-        patientData = (Button) findViewById(R.id.pdata);
-        emergency = (Button) findViewById(R.id.emergency);
+        vehicle = findViewById(R.id.vehicle);
+        patientData = findViewById(R.id.pdata);
+        emergency = findViewById(R.id.emergency);
         vehicle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,11 +34,28 @@ public class operationActivity extends AppCompatActivity {
         emergency.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage("9786285478", null, "", null, null);
+                sendEmail();
             }
         });
 
+    }
+
+    private void sendEmail() {
+        String[] To = {"ssrivishnu80@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, To);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Emergency");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(operationActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
